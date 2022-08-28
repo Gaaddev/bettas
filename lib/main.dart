edc0ed72194ef73c3f76edf18883e0f4f7a588c6
Gaaddev <gaaddev@gmail.com>
2022-08-28 16:23:27 +0100

    homepage begin

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
- 
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  theme: ThemeData(),
+  home: HomePage()
+  ,));
