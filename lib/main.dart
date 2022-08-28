cee4aceeaee1a4e96bd2c5e1f15b8942c07e274e
Gaaddev <gaaddev@gmail.com>
2022-08-28 16:32:27 +0100

    reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
-  theme: ThemeData(),
-  home: HomePage()
-  ,));
+    theme: ThemeData(
+    colorScheme: ColorScheme.fromSwatch(
+      primarySwatch: Colors.red,
+    ).copyWith(
+      secondary: Colors.grey,
+    ),
+    textTheme: const TextTheme(bodyText2: TextStyle(color: Colors.black)),
+  ),
+  home: HomePage(),
+  ));
