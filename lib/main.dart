cd8256b094f13606e9abfb81fe8af78d44702525
Gaaddev <gaaddev@gmail.com>
2022-08-30 02:05:34 +0100

    AppBar HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import '../pages/HomePage.dart';
+
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-    colorScheme: ColorScheme.fromSwatch(
-      primarySwatch: Colors.red,
-    ).copyWith(
-      secondary: Colors.grey,
-    ),
-    textTheme: const TextTheme(bodyText2: TextStyle(color: Colors.black)),
-  ),
-  home: HomePage(),
-  ));
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.grey,
+        ).copyWith(
+          secondary: Colors.red,
+        ),
+        textTheme: const TextTheme(bodyText2: TextStyle(color: Colors.black)),
+      ),
+      home: HomePage(),
+    ));
